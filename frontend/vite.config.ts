7d3221c0ed004e281ee6b9fe6459cd079238ba43
tuy250 <dieudonnetuy250@gmail.com>
2025-10-07 19:28:38 +0200

    first commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'build',
     sourcemap: false,
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
